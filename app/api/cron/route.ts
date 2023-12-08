5c9b28fc3b91695d3d3ba16c374cd20fcc723b75
Sahib.Aujla <sahibpreet568@gmail.com>
2023-12-08 13:11:27 -0500

    fixed error

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,7 +5,7 @@ import { connectToDB } from "@/lib/scraper/mongoose";
 import { getEmailNotifType } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 9; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
